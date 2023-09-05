863de79a3bcbc01bbb559fe6a2231c69324ee1ef
Sandeep <sandeep.utti@jbhunt.com>
2023-09-05 15:35:13 +0530

    minor

--- src/main/java/com/example/practiceproject/demo/config/EmployeeDetailsService.java
+++ src/main/java/com/example/practiceproject/demo/config/EmployeeDetailsService.java
@@ -18,21 +18,14 @@ public class EmployeeDetailsService implements UserDetailsService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    private PasswordEncoder encoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
         Optional<Employee> employeeinfo = employeeRepository.findByLastName(username);
         return employeeinfo.map(EmployeeInfoEmployeeDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException("user not found" + username));
-        //throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
     }
 
-    // public String addEmployee(Employee employee) {
-    //     employee.setPassword(encoder.encode(employee.getPassword()));
-    //     employeeRepository.save(employee);
-    //     return "User Added Successfully";
-    // }
+
     
 }
